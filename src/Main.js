4203fc998eea4858974b3385563500b59e8350fd
pablococciaglia <50939772+pablococciaglia@users.noreply.github.com>
2021-06-02 10:50:58 +0200

    corrections and comments
    corrections and comments

--- src/Main.js
+++ src/Main.js
@@ -4,11 +4,16 @@ import { Head } from './components/Select/Head'
 export const Main = () => {
    
     const [select, setSelect] = useState(null)
-       
+    /* useState: it will manage the selection from the selector 
+       in the Sort component, and send it to the Game compoenent 
+       to display the games list in the selected order */
     return (
-        <div>
-            <Head  setSelect={setSelect} select={select}/>
-            {<Games  select={select} /> }           
-        </div>
+        <>
+            <Head  setSelect={setSelect} select={select}/>,
+            <Games  select={select} />          
+        </>
     )
+    /* render the 2 main components of the site
+    Head: where is the title and the selector
+    Games: where is rendered the list of components */
 }
