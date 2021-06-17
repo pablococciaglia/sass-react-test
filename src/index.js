08fa20802d7d8925da776c76f24e952e1e9b218f
pablococciaglia <50939772+pablococciaglia@users.noreply.github.com>
2021-06-17 18:21:33 +0200

    sass
    correccion de sintaxis sass

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Main } from './Main';
-import './styles/styles.scss';
+import './sass/styles.scss';
 ReactDOM.render(
   <>
     <Main />
